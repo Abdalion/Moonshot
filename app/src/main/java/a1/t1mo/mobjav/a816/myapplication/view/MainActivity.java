f690d7e9037f432ad40f695f8d28c8bd5442b7d0
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2016-10-21 21:36:31 -0300

    Arme la estructura del proyecto y agregue datos de algunas pelis

--- app/src/main/java/a1/t1mo/mobjav/a816/myapplication/view/MainActivity.java
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/view/MainActivity.java
@@ -1,7 +1,9 @@
-package a1.t1mo.mobjav.a816.myapplication;
+package a1.t1mo.mobjav.a816.myapplication.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import a1.t1mo.mobjav.a816.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
