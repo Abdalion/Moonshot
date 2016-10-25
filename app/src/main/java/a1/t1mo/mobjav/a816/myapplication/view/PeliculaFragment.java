d88c5245e03ff71b885580710ad1ec99ea6b430a
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2016-10-25 11:51:54 -0300

    Trabajando en el RecyclerView

---
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/view/PeliculaFragment.java
@@ -0,0 +1,20 @@
+package a1.t1mo.mobjav.a816.myapplication.view;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import a1.t1mo.mobjav.a816.myapplication.R;
+
+public class PeliculaFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pelicula, container, false);
+    }
+
+}
