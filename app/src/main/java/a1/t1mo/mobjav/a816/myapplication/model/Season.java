0e8f07d121758bbcbbf2e86edc51cd332eb07d06
Unknown <tomasdelpup@hotmail.com>
2016-10-31 17:28:15 -0300

    Agregadas mas cosas

---
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/model/Season.java
@@ -0,0 +1,21 @@
+package a1.t1mo.mobjav.a816.myapplication.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by dh-mob-tt on 31/10/16.
+ */
+public class Season {
+
+    @SerializedName("air_date")
+    public String airDate;
+    @SerializedName("episode_count")
+    public Integer episodeCount;
+    @SerializedName("id")
+    public Integer id;
+    @SerializedName("poster_path")
+    public String posterPath;
+    @SerializedName("season_number")
+    public Integer seasonNumber;
+
+}
--- app/src/main/java/a1/t1mo/mobjav/a816/myapplication/model/Serie.java
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/model/Serie.java
@@ -1,5 +1,10 @@
 package a1.t1mo.mobjav.a816.myapplication.model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * MoonShot App
  * Proyecto Integrador
@@ -8,5 +13,52 @@ package a1.t1mo.mobjav.a816.myapplication.model;
  * Archivo creado por Juan Pablo on 29/10/2016.
  */
 
+
 public class Serie {
+
+    @SerializedName("backdrop_path")
+    public String backdropPath;
+    @SerializedName("first_air_date")
+    public String firstAirDate;
+    @SerializedName("genres")
+    public List<Genre> generos = new ArrayList<Genre>();
+    @SerializedName("homepage")
+    public String homepage;
+    @SerializedName("id")
+    public Integer id;
+    @SerializedName("in_production")
+    public Boolean inProduction;
+    @SerializedName("languages")
+    public List<String> languages = new ArrayList<String>();
+    @SerializedName("last_air_date")
+    public String lastAirDate;
+    @SerializedName("name")
+    public String name;
+    @SerializedName("number_of_episodes")
+    public Integer numberOfEpisodes;
+    @SerializedName("number_of_seasons")
+    public Integer numberOfSeasons;
+    @SerializedName("origin_country")
+    public List<String> originCountry = new ArrayList<String>();
+    @SerializedName("original_language")
+    public String originalLanguage;
+    @SerializedName("original_name")
+    public String originalName;
+    @SerializedName("overview")
+    public String overview;
+    @SerializedName("popularity")
+    public Double popularity;
+    @SerializedName("poster_path")
+    public String posterPath;
+    @SerializedName("seasons")
+    public List<Season> seasons = new ArrayList<Season>();
+    @SerializedName("status")
+    public String status;
+    @SerializedName("type")
+    public String type;
+    @SerializedName("vote_average")
+    public Double voteAverage;
+    @SerializedName("vote_count")
+    public Integer voteCount;
+
 }
