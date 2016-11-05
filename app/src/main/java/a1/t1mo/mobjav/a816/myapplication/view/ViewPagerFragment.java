7debbc27a302cb9931f70ed74020f0367300c29c
Abdalion <egonbush@hotmail.com>
2016-11-05 19:50:17 -0300

    ViewPagerFragment agregado. Cleanup XML main

---
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/view/ViewPagerFragment.java
@@ -0,0 +1,30 @@
+package a1.t1mo.mobjav.a816.myapplication.view;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import a1.t1mo.mobjav.a816.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ViewPagerFragment extends Fragment {
+
+
+    public ViewPagerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_view_pager, container, false);
+    }
+
+}
