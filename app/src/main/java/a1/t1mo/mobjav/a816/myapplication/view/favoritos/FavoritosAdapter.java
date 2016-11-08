357493d2df54b2b1d4f0b3f42311ad9a59d9cb41
Unknown <egonbush@hotmail.com>
2016-11-08 17:06:20 -0300

    Avance favoritos

---
+++ app/src/main/java/a1/t1mo/mobjav/a816/myapplication/view/favoritos/FavoritosAdapter.java
@@ -0,0 +1,22 @@
+package a1.t1mo.mobjav.a816.myapplication.view.favoritos;
+
+import a1.t1mo.mobjav.a816.myapplication.controller.PeliculaController;
+import a1.t1mo.mobjav.a816.myapplication.controller.SerieController;
+import a1.t1mo.mobjav.a816.myapplication.model.Genre;
+
+public class FavoritosAdapter {
+
+    private PeliculaController peliculaController;
+    private SerieController serieController;
+
+
+    public FavoritosFragment(Integer genero) {
+        peliculaController = new PeliculaController();
+        if (genero == 0) {
+            peliculaController.getFavoritos();
+        } else {
+            serieController.getFavoritos();
+        }
+    }
+
+}
